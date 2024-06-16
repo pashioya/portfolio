782c5c6adfff78588ea9dc21e9c40a9fcbf0a396
pashioya <john.ashioya@gmail.com>
2024-06-16 17:48:10 +0200

    contact me form

---
+++ components/ContactForm.tsx
@@ -0,0 +1,109 @@
+'use client';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { Textarea } from '@/components/ui/textarea';
+
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+
+const formSchema = z.object({
+  firstName: z.string().min(2, {
+    message: 'Username must be at least 2 characters.',
+  }),
+  lastName: z.string().min(2, {
+    message: 'Username must be at least 2 characters.',
+  }),
+  email: z.string().email({
+    message: 'Please enter a valid email address.',
+  }),
+  message: z.string().min(10, {
+    message: 'Message must be at least 10 characters.',
+  }),
+});
+
+export function ContactForm() {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      firstName: '',
+      lastName: '',
+      email: '',
+      message: '',
+    },
+  });
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(() => console.log('submit'))} className="space-y-8">
+        <div className="flex flex-row gap-6">
+          <FormField
+            control={form.control}
+            name="firstName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>First Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="John" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="lastName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Last Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="Doe" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email Address</FormLabel>
+              <FormControl>
+                <Input placeholder="random@gmail.com" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="message"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Message</FormLabel>
+              <FormControl>
+                <Textarea {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+}
--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -1,13 +1,14 @@
-import DialogComponent from "./DialogComponent";
-import { Button } from "./ui/button";
+import { ContactForm } from './ContactForm';
+import DialogComponent from './DialogComponent';
+import { Button } from './ui/button';
 
 export default function ContactMe() {
   return (
     <DialogComponent
-      title={"Contact Me"}
-      description={"Contact me here"}
-      trigger={<Button size={"lg"}>Contact Me</Button>}
-      content={undefined}
+      title={'Contact Me'}
+      description={'Contact me here'}
+      trigger={<Button size={'lg'}>Contact Me</Button>}
+      content={<ContactForm />}
       footer={undefined}
     />
   );
