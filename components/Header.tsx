7420d0a3d254749296e8fd168928690ba7bd8099
pashioya <john.ashioya@gmail.com>
2024-06-17 16:03:17 +0200

    added progress indicator

--- components/Header.tsx
+++ components/Header.tsx
@@ -44,73 +44,75 @@ export default function Header({ activeNav }: HeaderProps) {
   }
 
   return (
-    <>
-      <Navbar
-        classNames={{
-          item: [
-            'flex',
-            'relative',
-            'h-full',
-            'items-center',
-            "data-[active=true]:after:content-['']",
-            'data-[active=true]:after:absolute',
-            'data-[active=true]:after:bottom-0',
-            'data-[active=true]:after:left-0',
-            'data-[active=true]:after:right-0',
-            'data-[active=true]:after:h-[2px]',
-            'data-[active=true]:after:rounded-[2px]',
-            'data-[active=true]:after:bg-primary',
-          ],
-        }}
-      >
-        <NavbarBrand className="gap-2">
-          <Image src="/images/profile.png" alt="logo" width={40} height={40} />
-          <p className="font-bold text-inherit">Paul Ashioya</p>
-        </NavbarBrand>
-        <NavbarContent className="hidden sm:flex gap-4" justify="center">
-          <NavbarItem isActive={activeNav === 'home'}>
-            <Link color="foreground" href="#" onClick={() => scrollToHome()}>
-              Home
-            </Link>
-          </NavbarItem>
-          <NavbarItem isActive={activeNav === 'about-me'}>
-            <Link href="#" aria-current="page" onClick={() => scrollToAboutMe()}>
-              About
-            </Link>
-          </NavbarItem>
-          <NavbarItem>
-            <Link color="foreground" href="#">
-              Projects
-            </Link>
-          </NavbarItem>
-        </NavbarContent>
-        <NavbarContent justify="end">
-          {isDesktop ? (
-            <DropdownMenu>
-              <DropdownMenuTrigger className="p-1">
-                {theme === 'dark' ? <Moon /> : <Sun />}
-              </DropdownMenuTrigger>
-              <DropdownMenuContent>
-                <DropdownMenuGroup>
-                  <DropdownMenuItem
-                    onMouseUp={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
-                    className="items-center justify-center"
-                  >
-                    {theme === 'dark' ? <Sun /> : <Moon />}
-                  </DropdownMenuItem>
-                </DropdownMenuGroup>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          ) : (
-            <Sheet>
-              <SheetTrigger>
-                <Menu />
-              </SheetTrigger>
-              <SheetContent></SheetContent>
-            </Sheet>
-          )}
-        </NavbarContent>
-      </Navbar>
-    </>
+    <Navbar
+      shouldHideOnScroll
+      position="static"
+      classNames={{
+        item: [
+          'flex',
+          'relative',
+          'z-[100]',
+          'top-0',
+          'h-full',
+          'items-center',
+          "data-[active=true]:after:content-['']",
+          'data-[active=true]:after:absolute',
+          'data-[active=true]:after:bottom-0',
+          'data-[active=true]:after:left-0',
+          'data-[active=true]:after:right-0',
+          'data-[active=true]:after:h-[2px]',
+          'data-[active=true]:after:rounded-[2px]',
+          'data-[active=true]:after:bg-primary',
+        ],
+      }}
+    >
+      <NavbarBrand className="gap-2">
+        <Image src="/images/profile.png" alt="logo" width={40} height={40} />
+        <p className="font-bold text-inherit">Paul Ashioya</p>
+      </NavbarBrand>
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarItem isActive={activeNav === 'home'}>
+          <Link color="foreground" href="#" onClick={() => scrollToHome()}>
+            Home
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive={activeNav === 'about-me'}>
+          <Link href="#" aria-current="page" onClick={() => scrollToAboutMe()}>
+            About
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Projects
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        {isDesktop ? (
+          <DropdownMenu>
+            <DropdownMenuTrigger className="p-1">
+              {theme === 'dark' ? <Moon /> : <Sun />}
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuGroup>
+                <DropdownMenuItem
+                  onMouseUp={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+                  className="items-center justify-center"
+                >
+                  {theme === 'dark' ? <Sun /> : <Moon />}
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        ) : (
+          <Sheet>
+            <SheetTrigger>
+              <Menu />
+            </SheetTrigger>
+            <SheetContent></SheetContent>
+          </Sheet>
+        )}
+      </NavbarContent>
+    </Navbar>
   );
 }
